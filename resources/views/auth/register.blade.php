a978a62e1188734651182ec6fe994a8333cb3d4d
Aliie25 <182410102033@students.unej.ac.id>
2021-03-02 16:50:33 +0700

    Update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,29 @@
+<<<<<<< Updated upstream
 {{-- @extends('layouts.css')
 
 <body class="bg-gradient-primary">
 
+=======
+@extends('layouts.css')
+<div id="app">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                E-ARSIP
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+                </ul>
+            </div>
+        </div>
+    </div>        
+<body class="bg-gradient-light">                
+>>>>>>> Stashed changes
     <div class="container">
 
         <div class="card o-hidden border-0 shadow-lg my-5">
