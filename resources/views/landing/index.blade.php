dbdd23957d1ce5336af2d3bdc09c1041f127f63b
AryaRamadhan24 <arya.ramadhan2448@gmail.com>
2021-03-03 17:09:41 +0700

    update

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -69,14 +69,14 @@
         <div class="row">
           <div class="col-lg-3 col-md-6">
             <div class="member" data-aos="fade-up" data-aos-delay="100">
-              <div class="pic"><img src="assets/img/team-1.jpg" alt=""></div>
-              <h4>Tri</h4>
+              <div class="pic"><img src="{{asset('Landingpage/img/Kominfo.jpg')}}" alt=""></div>
+              <h4>Tri D.S</h4>
               <span>Pembimbing</span>
               <div class="social">
-                <a href=""><i class="fa fa-twitter"></i></a>
-                <a href=""><i class="fa fa-facebook"></i></a>
-                <a href=""><i class="fa fa-google-plus"></i></a>
-                <a href=""><i class="fa fa-linkedin"></i></a>
+                <a href="https://twitter.com/KominfoMalang?s=20"><i class="fa fa-twitter"></i></a>
+                <a href="https://www.facebook.com/kominfomalang/"><i class="fa fa-facebook"></i></a>
+                <a href="mailto:kominfo@malangkota.go.id"><i class="fa fa-google-plus"></i></a>
+                <a href="https://www.instagram.com/kominfomalang/"><i class="fa fa-instagram"></i></a>
               </div>
             </div>
           </div>
@@ -88,9 +88,9 @@
               <span>Project Manager</span>
               <div class="social">
                 <a href=""><i class="fa fa-twitter"></i></a>
-                <a href=""><i class="fa fa-facebook"></i></a>
-                <a href=""><i class="fa fa-google-plus"></i></a>
-                <a href=""><i class="fa fa-linkedin"></i></a>
+                <a href="https://www.facebook.com/arya.ramadhan.5030"><i class="fa fa-facebook"></i></a>
+                <a href="mailto:182410102035@cs.unej.ac.id"><i class="fa fa-google-plus"></i></a>
+                <a href="https://www.instagram.com/aryaramadhan24/"><i class="fa fa-instagram"></i></a>
               </div>
             </div>
           </div>
@@ -101,10 +101,10 @@
               <h4>Saifur Rifqi Ali</h4>
               <span>Front-end Developer</span>
               <div class="social">
-                <a href=""><i class="fa fa-twitter"></i></a>
-                <a href=""><i class="fa fa-facebook"></i></a>
-                <a href=""><i class="fa fa-google-plus"></i></a>
-                <a href=""><i class="fa fa-linkedin"></i></a>
+                <a href="https://twitter.com/Aliie_25"><i class="fa fa-twitter"></i></a>
+                <a href="https://www.facebook.com/riefqhi.alhie"><i class="fa fa-facebook"></i></a>
+                <a href="mailto:saifurrifqi9@gmail.com"><i class="fa fa-google-plus"></i></a>
+                <a href="https://www.instagram.com/aliie_25/"><i class="fa fa-instagram"></i></a>
               </div>
             </div>
           </div>
@@ -116,9 +116,9 @@
               <span>Back-end Developer</span>
               <div class="social">
                 <a href=""><i class="fa fa-twitter"></i></a>
-                <a href=""><i class="fa fa-facebook"></i></a>
+                <a href="https://www.facebook.com/profile.php?id=100009883663580"><i class="fa fa-facebook"></i></a>
                 <a href=""><i class="fa fa-google-plus"></i></a>
-                <a href=""><i class="fa fa-linkedin"></i></a>
+                <a href="https://www.instagram.com/miantoko_gundho/"><i class="fa fa-instagram"></i></a>
               </div>
             </div>
           </div>
@@ -126,84 +126,6 @@
 
       </div>
     </section><!-- End Team Section -->
-
-    {{-- <!-- ======= Contact Section ======= -->
-    <section id="contact">
-      <div class="container">
-        <div class="section-header">
-          <h3 class="section-title">Contact</h3>
-          <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
-        </div>
-      </div>
-
-      <!-- Uncomment below if you wan to use dynamic maps -->
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22864.11283411948!2d-73.96468908098944!3d40.630720240038435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY%2C+USA!5e0!3m2!1sen!2sbg!4v1540447494452" width="100%" height="380" frameborder="0" style="border:0" allowfullscreen></iframe>
-
-      <div class="container mt-5">
-        <div class="row justify-content-center">
-
-          <div class="col-lg-3 col-md-4">
-
-            <div class="info">
-              <div>
-                <i class="fa fa-map-marker"></i>
-                <p>A108 Adam Street<br>New York, NY 535022</p>
-              </div>
-
-              <div>
-                <i class="fa fa-envelope"></i>
-                <p>info@example.com</p>
-              </div>
-
-              <div>
-                <i class="fa fa-phone"></i>
-                <p>+1 5589 55488 55s</p>
-              </div>
-            </div>
-
-            <div class="social-links">
-              <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-              <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
-              <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
-              <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
-              <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
-            </div>
-
-          </div>
-
-          <div class="col-lg-5 col-md-8">
-            <div class="form">
-              <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-                <div class="form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                  <div class="validate"></div>
-                </div>
-                <div class="form-group">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                  <div class="validate"></div>
-                </div>
-                <div class="form-group">
-                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                  <div class="validate"></div>
-                </div>
-                <div class="form-group">
-                  <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-                  <div class="validate"></div>
-                </div>
-                <div class="mb-3">
-                  <div class="loading">Loading</div>
-                  <div class="error-message"></div>
-                  <div class="sent-message">Your message has been sent. Thank you!</div>
-                </div>
-                <div class="text-center"><button type="submit">Send Message</button></div>
-              </form>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-    </section><!-- End Contact Section --> --}}
 
   </main><!-- End #main -->
 
